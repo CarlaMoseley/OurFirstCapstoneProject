52c25803b65d2a7f16e56a4b4dfa76924cbf071c
MacMaster <andrew.macmaster@fiserv.com>
2023-12-20 11:30:10 -0500

    Added logic for all tenant routes except login/signup/payment processing

--- Capstone/tenant/routes.py
+++ Capstone/tenant/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 from flask import current_app as app
+from ..models import Tenant, Unit, Payment
 
 # Blueprint for tenant
 tenant_bp = Blueprint(
@@ -27,24 +28,40 @@ def tenant_signup():
 @tenant_bp.route('/tenant/<int:tenant_id>')
 def tenant_profile(tenant_id):
     # render tenant profile page
-    pass
+    tenant = Tenant.query.filter_by(id=tenant_id).first()
+
+    return render_template('PLACEHOLDER', tenant=tenant)
 
 @tenant_bp.route('/tenant/<int:tenant_id>/payments')
 def tenant_payments(tenant_id):
     # render tenant payments page
-    pass
+    tenant = Tenant.query.filter_by(id=tenant_id).first()
+    payments = tenant.payments
+
+    return render_template('PLACEHOLDER', tenant=tenant, payments=payments)
 
 @tenant_bp.route('/tenant/<int:tenant_id>/unit')
 def tenant_unit(tenant_id):
     # render tenant unit page
-    pass
+    tenant = Tenant.query.filter_by(id=tenant_id).first()
+    unit = Unit.query.filter_by(tenant.unit_id).first()
 
-@tenant_bp.route('/tenant/<int:tenant_id>/makepayment')
+    return render_template('PLACEHOLDER', tenant=tenant, unit=unit)
+
+@tenant_bp.route('/tenant/<int:tenant_id>/makepayment', methods=['GET', 'POST'])
 def make_payment(tenant_id):
     # render make payment page
-    pass
+    tenant = Tenant.query.filter_by(id=tenant_id).first()
+    if request.method == 'GET':
+        return render_template('PLACEHOLDER', tenant=tenant)
+    elif request.method == 'POST':
+        # insert logic about payment processing here
+        pass
 
 @tenant_bp.route('/tenant/<int:tenant_id>/<int:payment_id>')
 def tenant_payment(tenant_id, payment_id):
     # render individual payment page
-    pass
+    tenant = Tenant.query.filter_by(id=tenant_id).first()
+    payment = Payment.query.filter_by(id=payment_id).first
+
+    return render_template('PLACEHOLDER', tenant=tenant, payment=payment)
